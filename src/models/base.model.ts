cd3d6f39f735f2788dc1d198f0b11316126cec9e
Choeng Kimlay <mingfongmen@gmail.com>
2025-01-17 14:08:33 +0700

    Change user id from string to number

--- src/models/base.model.ts
+++ src/models/base.model.ts
@@ -4,12 +4,10 @@ import { AutoIncrement, Column, Model, Table } from "sequelize-typescript";
 export class BaseModel<T> extends Model<T> {
     @AutoIncrement
     @Column({ primaryKey: true, unique: true })
-    id?: string;
-
-
+    id?: number;
 
     /**
-     * * the postgres database will auto create this column for us incase they didnt you can uncomment this
+     * * the postgres database will auto create this column for us incase they didn't you can uncomment this
      */
     // @Column({ defaultValue: new Date() })
     // createdAt?: Date;
